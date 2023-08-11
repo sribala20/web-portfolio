523213be743661a482a014ab959f9ac8bd728983
sribala20 <sribala455@gmail.com>
2023-08-10 21:57:20 -0700

    retry deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,8 @@
-
 import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
 
 export default function Navbar() {
-  return ( //md:sticky helps it stick to the top of the page
+  return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
 import { testimonials } from "../data";
