ef40f2fb061e9af152a2150a7e1ec4922c82bc95
sribala20 <sribala455@gmail.com>
2023-08-22 18:32:01 -0700

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,27 @@
-import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
 
 export default function Navbar() {
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-10">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
-            Reed Barger
+    <header className="bg-lilac md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-4 md:flex-nowrap items-center justify-between"> {/* Changed justify-start to justify-between */}
+        <div className="font-sans semibold text-gray-800 md:mb-0">
+          <a href="#about" className="ml-auto text-xl font-sans">
+            Sri Bala
           </a>
-        </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
-            Past Work
+        </div>
+        <nav className="md:ml-auto md:mr-1 md:py-1 md:pr-4 flex inline-flex items-center text-base justify-end md:flex-nowrap"> {/* Changed items-right to items-center */}
+          <a href="#projects" className="mr-7 text-gray-500 hover:text-gray-900">
+            Development
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
+          <a href="#skills" className="mr-7 text-gray-500 hover:text-gray-900">
             Skills
           </a>
-          <a href="#testimonials" className="mr-5 hover:text-white">
-            Testimonials
-          </a>
+          <div class="bg-gray-700 rounded-xl py-2 px-2 text-center hover:bg-gray-600">
+            <a href="#contact" className="block text-gray-100">
+              Contact
+            </a>
+          </div>
         </nav>
-        <a
-          href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
-          Hire Me
-          <ArrowRightIcon className="w-4 h-4 ml-1" />
-        </a>
       </div>
     </header>
   );
